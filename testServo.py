5200001d8bbf755be18da7239b0e5f605fcd9c46
Simon Wellnhofer <simon-wellnhofer@gmx.de>
2023-06-27 16:39:02 +0200

    Servo

--- testServo.py
+++ testServo.py
@@ -1,32 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 
-servo_pin = 18  # GPIO pin number where the servo is connected
-
+servoPIN = 4
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-servo_pwm = GPIO.PWM(servo_pin, 50)  # 50 Hz (20 ms period)
-
-def set_speed(speed):
-    if speed < 0:
-        servo_pwm.ChangeDutyCycle(0)  # Stop rotating
-    else:
-        duty_cycle = speed * 2 + 6  # Map speed (-1 to 1) to duty cycle (4-10%)
-        servo_pwm.ChangeDutyCycle(duty_cycle)
-
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
+p.start(2.5) # Initialisierung
 try:
-    while True:
-        speed = float(input("Enter the desired speed (-1 to 1, q to quit): "))
-        if speed < -1 or speed > 1:
-            print("Invalid speed. Please enter a value between -1 and 1.")
-            continue
-        if speed == 'q':
-            break
-        set_speed(speed)
-
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
 except KeyboardInterrupt:
-    pass
-
-servo_pwm.stop()
-GPIO.cleanup()
+  p.stop()
+  GPIO.cleanup()
