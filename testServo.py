d7019f13b4ebb7798c191a2d8289f1b9f53bb60e
Simon Wellnhofer <simon-wellnhofer@gmx.de>
2023-07-04 10:14:36 +0200

    testServ2981o

--- testServo.py
+++ testServo.py
@@ -10,7 +10,7 @@ p.start(0)  # Initialization
 
 rotation_time = 0.5  # Time in seconds for one complete rotation (adjust as needed)
 initial_delay = 2.0  # Time in seconds for initial delay (adjust as needed)
-
+speed = 0.5
 try:
     # Wait for the servo to reach its starting position
     time.sleep(initial_delay)
@@ -18,24 +18,30 @@ try:
     while True:
         position = input("Enter position (1-6): ")
         if position == '1':
-            p.ChangeDutyCycle(12.5)  # Full speed clockwise
-            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5 + speed)  # Full speed clockwise
+            time.sleep(2.5 * rotation_time)
             p.ChangeDutyCycle(0)  # Stop
         elif position == '2':
-            p.ChangeDutyCycle(10)  # Medium speed clockwise
-            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5 + speed)  # Medium speed clockwise
+            time.sleep(1.5 * rotation_time)
             p.ChangeDutyCycle(0)  # Stop
         elif position == '3':
-            p.ChangeDutyCycle(7.5)  # Stop (middle position)
+            p.ChangeDutyCycle(7.5 + speed)  # Medium speed clockwise
+            time.sleep(0.5 * rotation_time)
+            p.ChangeDutyCycle(0)  # Stop
         elif position == '4':
-            p.ChangeDutyCycle(5)  # Medium speed counterclockwise
-            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5 - speed)
+            time.sleep(0.5 * rotation_time)
             p.ChangeDutyCycle(0)  # Stop
         elif position == '5':
-            p.ChangeDutyCycle(2.5)  # Full speed counterclockwise
-            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5 - speed)
+            time.sleep(1.5 * rotation_time)
             p.ChangeDutyCycle(0)  # Stop
-        elif position == '6':
+        elif position == '5':
+            p.ChangeDutyCycle(7.5 - speed)
+            time.sleep(2.5 * rotation_time)
+            p.ChangeDutyCycle(0)  # Stop
+        elif position == '7':
             break  # Exit the loop if position 6 is selected
         else:
             print("Invalid position. Please enter a number between 1 and 6.")
