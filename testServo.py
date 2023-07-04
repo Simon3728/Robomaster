b00ba5a101499631753bdc6df27e1ef3141ccfc4
Simon Wellnhofer <simon-wellnhofer@gmx.de>
2023-07-04 10:00:13 +0200

    testServ21o

--- testServo.py
+++ testServo.py
@@ -15,24 +15,34 @@ try:
         position = input("Enter position (1-6): ")
         if position == '1':
             p.ChangeDutyCycle(12.5)  # Full speed clockwise
+            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5)  # Stop
         elif position == '2':
             p.ChangeDutyCycle(10)  # Medium speed clockwise
+            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5)  # Stop
         elif position == '3':
             p.ChangeDutyCycle(7.5)  # Stop (middle position)
         elif position == '4':
             p.ChangeDutyCycle(5)  # Medium speed counterclockwise
+            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5)  # Stop
         elif position == '5':
             p.ChangeDutyCycle(2.5)  # Full speed counterclockwise
+            time.sleep(rotation_time)
+            p.ChangeDutyCycle(7.5)  # Stop
         elif position == '6':
             break  # Exit the loop if position 6 is selected
         else:
             print("Invalid position. Please enter a number between 1 and 6.")
             continue
 
-        time.sleep(rotation_time)  # Sleep for the rotation time
-
 except KeyboardInterrupt:
     pass
+
+p.stop()
+GPIO.cleanup()
+
 
 p.stop()
 GPIO.cleanup()
