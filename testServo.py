b1b319a20cb3afcf95b719e3652963d38dbed33a
Simon Wellnhofer <simon-wellnhofer@gmx.de>
2023-07-04 09:58:33 +0200

    testServ76o

--- testServo.py
+++ testServo.py
@@ -6,20 +6,34 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50)  # GPIO 17 as PWM with 50Hz
-p.start(2.5)  # Initialization
+p.start(7.5)  # Initialization at the middle position
 
-rotation_time = 2.0  # Time in seconds for one complete rotation (adjust as needed)
+rotation_time = 0.5  # Time in seconds for one complete rotation (adjust as needed)
 
 try:
-    p.ChangeDutyCycle(3)  # Set the duty cycle for the desired position
-    time.sleep(rotation_time)  # Sleep for the rotation time
-    p.ChangeDutyCycle(2.5)
-    time.sleep(rotation_time)
-    p.ChangeDutyCycle(10)  # Set the duty cycle for the desired position
-    time.sleep(rotation_time)  # Sleep for the rotation time
-    p.ChangeDutyCycle(2.5)
+    while True:
+        position = input("Enter position (1-6): ")
+        if position == '1':
+            p.ChangeDutyCycle(12.5)  # Full speed clockwise
+        elif position == '2':
+            p.ChangeDutyCycle(10)  # Medium speed clockwise
+        elif position == '3':
+            p.ChangeDutyCycle(7.5)  # Stop (middle position)
+        elif position == '4':
+            p.ChangeDutyCycle(5)  # Medium speed counterclockwise
+        elif position == '5':
+            p.ChangeDutyCycle(2.5)  # Full speed counterclockwise
+        elif position == '6':
+            break  # Exit the loop if position 6 is selected
+        else:
+            print("Invalid position. Please enter a number between 1 and 6.")
+            continue
+
+        time.sleep(rotation_time)  # Sleep for the rotation time
+
 except KeyboardInterrupt:
     pass
 
 p.stop()
 GPIO.cleanup()
+
