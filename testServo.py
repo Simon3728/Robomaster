d6e33db875c4806a8ac123bd3ca3d463a590776b
Simon Wellnhofer <simon-wellnhofer@gmx.de>
2023-07-04 10:38:31 +0200

    test4

--- testServo.py
+++ testServo.py
@@ -4,7 +4,7 @@ import time
 servoPIN = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
-p = GPIO.PWM(servoPIN, 20)  # GPIO 17 as PWM with 50Hz
+p = GPIO.PWM(servoPIN, 50)  # GPIO 17 as PWM with 50Hz
 p.start(0)  # Initialization
 
 rotation_time = 0.5  # Time in seconds for one complete rotation (adjust as needed)
@@ -13,13 +13,13 @@ speed = 0.5
 try:
     # Wait for the servo to reach its starting position
     time.sleep(initial_delay)
-    i = 2
+    i = 2.7
     while True:
         p.ChangeDutyCycle(i)
         print(i)
         time.sleep(2)
-        i += 0.1
-        if i >= 3:
+        i += 0.0125
+        if i >= 2.8:
             break
 
 except KeyboardInterrupt:
